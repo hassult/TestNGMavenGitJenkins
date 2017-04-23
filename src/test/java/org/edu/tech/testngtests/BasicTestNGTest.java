b85dd1de805924d35f0cb3be2f154fc2619fd82b
Hasan Sultanov <hsultanov77@gmail.com>
2017-04-22 23:28:42 -0400

    new tests added

---
+++ src/test/java/org/edu/tech/testngtests/BasicTestNGTest.java
@@ -0,0 +1,19 @@
+package org.edu.tech.testngtests;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class BasicTestNGTest {
+	
+	@Test
+	public void test(){
+		Assert.assertEquals(2+3, 5);
+	}
+	
+	@Test
+	public void test2(){
+		Assert.assertEquals(2+3, 6);
+	}
+
+}
